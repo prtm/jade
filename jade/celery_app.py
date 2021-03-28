d22c3a5492ac4728db2b34b0870618f3d558f49a
Preetam Swami <android.preetam@gmail.com>
2021-03-28 18:27:12 +0530

    celery beat added scheduled task

--- jade/celery_app.py
+++ jade/celery_app.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jade.settings")
@@ -12,6 +13,15 @@ app = Celery("jade")
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.beat_schedule = {
+    # Executes every Day morning at 6:00 p.m.
+    'every_day_redis_bhav_copy_update': {
+        'task': 'market.tasks.update_bhav_data',
+        'schedule': crontab(hour=18),
+        'args': (),
+    },
+}
+app.conf.timezone = 'Asia/Calcutta'
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
