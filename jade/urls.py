1a79c6d27da4c08e218fec88886a5e92b5bf2e4c
Preetam Swami <android.preetam@gmail.com>
2021-03-28 18:26:18 +0530

    admin url commented

--- jade/urls.py
+++ jade/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('market/', include('market.urls')),
 ]
