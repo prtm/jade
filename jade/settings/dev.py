93ab2f798d012d6c7489bd45d8b5110313a06019
Preetam Swami <android.preetam@gmail.com>
2021-03-28 21:04:29 +0530

    adding whitenoise

--- jade/settings/dev.py
+++ jade/settings/dev.py
@@ -5,3 +5,8 @@ DEBUG = True
 INSTALLED_APPS += [
     "django_extensions",
 ]
+
+# WhiteNoise
+# ------------------------------------------------------------------------------
+# http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
+INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa F405
--- jade/settings/prod.py
+++ jade/settings/prod.py
@@ -5,3 +5,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",)
 }
+
+# STATIC
+# ------------------------
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
